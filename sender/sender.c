a6468e377993b0c081388e26cd5e55f8647f02e9
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-04-05 22:40:47 +0530

    Update sender.c

--- sender/sender.c
+++ sender/sender.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <sender.h>
 
-void printOnConsole()
+void ReadBMSParametersFromFile(float* Temp, float* SOC)
 {
-  
+    float Temperature, StateOfCharge;
+    FILE * file= fopen("./sender/BMSData.txt","r");  
+    if (file!=NULL) 
+    {
+        for(int i=0;fscanf(file, "%f\t%f\t%f\n", &Temperature,&StateOfCharge)!=EOF ;i++)
+        {
+            *(Temp+i) = Temperature;
+            *(SOC+i)  = StateOfCharge;
+        }
+    }
+    fclose(file);  
+}
+
+void printBMSParamsOnConsole(float* Temp, float* SOC)
+{
+  char bmsParam[50];
+  for (i=0; i<NOOFSAMPLES; i++)
+  {
+    sprintf(bmsParam[50], "Temperature: %f, SOC: %f", *(Temp+i),*(SOC+i));
+  }
+}
+
+void sendBMSParameters(){
+  float Temperature[NOOFSAMPLES] = {0};
+  float StateOfCharge[NOOFSAMPLES]= {0};
+  ReadBMSParametersFromFile(Temperature, StateOfCharge);
+  printBMSParamsOnConsole(Temperature, StateOfCharge);
 }
