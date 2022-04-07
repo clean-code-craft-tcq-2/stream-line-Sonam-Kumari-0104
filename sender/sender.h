af42e18668b25e3aec606660a27b8b25c981216a
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-04-07 09:58:33 +0530

    Update sender.h

--- sender/sender.h
+++ sender/sender.h
@@ -1,10 +1,5 @@
 #define NOOFSAMPLES 50;
 
-typedef struct{
-float tempearture[NOOFSAMPLES];
-float SOC[NOOFSAMPLES];
-}batteryParameters;
-
 void ReadBMSParametersFromFile(float* Temp, float* SOC);
 void printBMSParamsOnConsole(float* Temp, float* SOC);
 void sendBMSParameters();
