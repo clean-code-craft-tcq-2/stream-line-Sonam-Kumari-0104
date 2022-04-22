1a00f768f638faf241945e5e0df2025e52709069
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-04-22 17:17:51 +0530

    integrated Sender-receiver

--- receiver/processingSenderData.c
+++ receiver/processingSenderData.c
@@ -7,15 +7,15 @@
 
 float parseParamValueFromString (char *input)
 {
-	int dest_index = 0;
+	int data_index = 0;
 	char parsed_value [10];
 	
-	while((input [dest_index] >= '0' && input [dest_index] <= '9') || input [dest_index] == '.')
+	while((input [data_index] >= '0' && input [data_index] <= '9') || input [data_index] == '.' || input [data_index] == '-')
 	{
-		parsed_value [dest_index] = input [dest_index];
-		dest_index++;
+		parsed_value [data_index] = input [data_index];
+		data_index++;
 	}
-	parsed_value[dest_index] = '\0';
+	parsed_value[data_index] = '\0';
 	
 	return atof(parsed_value);
 }
@@ -24,18 +24,18 @@ void parseSenderData (char *senderData, struct BMS *bms_param)
 {
 	const char* delim = ":";
 	char *parameterName = NULL;
-	int dest_index = 0;
+	int data_index = 0;
 	
 	parameterName = strtok (senderData, delim);
 	
-	dest_index = strlen (parameterName) + strlen (delim);
-	bms_param->temperature.value = parseParamValueFromString(&parameterName[dest_index]);
+	data_index = strlen (parameterName) + strlen (delim);
+	bms_param->temperature.value = parseParamValueFromString(&parameterName[data_index]);
 	parameterName = strtok (NULL, delim);
-	dest_index = strlen (parameterName) + strlen (delim);
-	bms_param->soc.value = parseParamValueFromString(&parameterName[dest_index]);
+	data_index = strlen (parameterName) + strlen (delim);
+	bms_param->soc.value = parseParamValueFromString(&parameterName[data_index]);
 	parameterName = strtok (NULL, delim);
-	dest_index = strlen (parameterName) + strlen (delim);
-	bms_param->chargeRate.value = parseParamValueFromString(&parameterName[dest_index]);
+	data_index = strlen (parameterName) + strlen (delim);
+	bms_param->chargeRate.value = parseParamValueFromString(&parameterName[data_index]);
 		
 	return;
 }
--- receiver/receiver.c
+++ receiver/receiver.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "processingSenderData.h"
-#define NO_OF_DATASETS 5
+#define NO_OF_DATASETS 50
+#define MAX_SIZE 128
 
 int main ()
 {
-    char Data1[] = "Temperature:2.00 deg C, State of Charge:5.00, Charge Rate:0.40\n"; 
-    char Data2[] = "Temperature:5.00 deg C, State of Charge:4.00, Charge Rate:0.10\n";
-    char Data3[] = "Temperature:8.00 deg C, State of Charge:10.00, Charge Rate:0.20\n";
-	char Data4[] = "Temperature:7.00 deg C, State of Charge:6.00, Charge Rate:0.30\n";
-	char Data5[] = "Temperature:4.00 deg C, State of Charge:7.00, Charge Rate:0.50\n";
-    char *senderData[NO_OF_DATASETS] = {Data1, Data2, Data3, Data4, Data5};
-	struct BMS bms_input = {0.0};
-	
+	struct BMS bms_input = {.temperature = {0.0, .temp_stat = {0.0, 0.0, 0.0, 0.0, 0.0}}, 
+							.soc = {0.0, .soc_stat = {0.0, 0.0, 0.0, 0.0, 0.0}}, 
+							.chargeRate = {0.0, .CR_stat = {0.0, 0.0, 0.0, 0.0, 0.0}}};
+	char senderData[MAX_SIZE] = {'\0'};
 	for (int dataSetIndex = 0; dataSetIndex < NO_OF_DATASETS; dataSetIndex++ )
 	{
-		parseSenderData (senderData[dataSetIndex], &bms_input);
+		fgets(senderData, MAX_SIZE, stdin);
+		parseSenderData (senderData, &bms_input);
 		findMinMaxOfBMSParams(&bms_input);
 		findMovAvgOfBMSParams(&bms_input);
+		memset(senderData, '\0', MAX_SIZE);
 	}
-	printf ("Minimum values are\n%f %f %f\n",bms_input.temperature.temp_stat.min, bms_input.soc.soc_stat.min, bms_input.chargeRate.CR_stat.min);
-	printf ("Maximum values are\n%f %f %f\n",bms_input.temperature.temp_stat.max, bms_input.soc.soc_stat.max, bms_input.chargeRate.CR_stat.max);
-	printf ("Mov Average %f %f %f\n", bms_input.temperature.temp_stat.movAvg, bms_input.soc.soc_stat.movAvg, bms_input.chargeRate.CR_stat.movAvg);
+	printf ("Minimum values are\n----------------\n%f %f %f\n",bms_input.temperature.temp_stat.min, bms_input.soc.soc_stat.min, bms_input.chargeRate.CR_stat.min);
+	printf ("Maximum values are\n----------------\n%f %f %f\n",bms_input.temperature.temp_stat.max, bms_input.soc.soc_stat.max, bms_input.chargeRate.CR_stat.max);
+	printf ("Mov Average\n----------------\n%f %f %f\n", bms_input.temperature.temp_stat.movAvg, bms_input.soc.soc_stat.movAvg, bms_input.chargeRate.CR_stat.movAvg);
 
-	return 0;
-	
+	return 0;	
 }
